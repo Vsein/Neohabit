094707da9faeacdf4346e831ec734486d828f194
Vsein <vseinssj@gmail.com>
2022-10-21 13:39:12 +0300

    Add a mobile menu for filters

--- src/UI/Sidebar.js
+++ src/UI/Sidebar.js
@@ -19,50 +19,75 @@ export default class Sidebar {
         </div>
       </div>
     `;
+    content.innerHTML += `
+      <div class="sidebar-mobile">
+        <div class="filters-mobile">
+        </div>
+        <div class="projects-mobile">
+        </div>
+      </div>
+    `;
   }
 
   static init() {
     Storage.getToDoList()
       .getFilters()
       .forEach((filter) => {
-        const filterSections = document.querySelector('.filters');
-        filterSections.appendChild(Sidebar.createFilterSection(filter));
+        Sidebar.createFilterSection(filter);
       });
 
     Storage.getToDoList()
       .getProjects()
       .forEach((project) => {
-        const projects = document.querySelector('.projects-container');
-        projects.appendChild(Sidebar.createProjectTile(project));
+        Sidebar.createProjectTile(project);
       });
   }
 
   static createFilterSection(filter) {
+    const filters = document.querySelector('.filters');
     const filterSection = document.createElement('div');
+    const filtersMobile = document.querySelector('.filters-mobile');
+    const filterSectionMobile = document.createElement('img');
+
     filterSection.classList.add('filter');
+    filterSectionMobile.classList.add('filter-mobile');
     if (filter.name === 'All') {
       filterSection.classList.add('active');
+      filterSectionMobile.classList.add('active');
     }
     if (filter.name === 'Important') {
       filterSection.classList.add('important');
+      filterSectionMobile.classList.add('important');
     }
 
     filterSection.innerHTML = `
       <img src=${filter.image} height="20px" width="20px">
       <p>${filter.name}<p>
     `;
+    filterSectionMobile.src = filter.image;
 
     filterSection.addEventListener('click', () => {
       Editor.changeListTo(filter.name);
       Sidebar.resetActiveSection();
 
       filterSection.classList.add('active');
+      filterSectionMobile.classList.add('active');
     });
 
-    return filterSection;
+    filterSectionMobile.addEventListener('click', () => {
+      Editor.changeListTo(filter.name);
+      Sidebar.resetActiveSection();
+
+      filterSection.classList.add('active');
+      filterSectionMobile.classList.add('active');
+    });
+
+    filters.appendChild(filterSection);
+    filtersMobile.appendChild(filterSectionMobile);
   }
 
   static createProjectTile(project) {
+    const projects = document.querySelector('.projects-container');
     const projectTile = document.createElement('div');
     projectTile.classList.add('project');
     projectTile.innerHTML += `
@@ -82,14 +107,17 @@ export default class Sidebar {
       projectTile.style.backgroundColor = `${project.color}33`;
     });
 
-    return projectTile;
+    projects.appendChild(projectTile);
   }
 
   static resetActiveSection() {
     const currentSection = document.querySelector('.sidebar .active');
+    const currentSectionMobile = document.querySelector('.sidebar-mobile .active');
     if (currentSection) {
       currentSection.classList.remove('active');
-      currentSection.style.removeProperty('background-color');
+    }
+    if (currentSectionMobile) {
+      currentSectionMobile.classList.remove('active');
     }
   }
 }
