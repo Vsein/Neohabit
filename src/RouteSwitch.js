e32839aa98f9f944115758dc678d31f4f03ae437
Vsein <vseinssj@gmail.com>
2022-10-26 21:56:40 +0300

    Add react routing to page generation

---
+++ src/RouteSwitch.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ToDo from './UI/ToDo';
+
+const RouteSwitch = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<ToDo />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default RouteSwitch;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import UI from './UI/UI';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RouteSwitch from './RouteSwitch';
 
-UI.load();
+const div = document.createElement('div');
+div.id = 'root';
+document.body.appendChild(div);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouteSwitch />
+  </React.StrictMode>
+);
