bfff3f3aa4844205095abe1fb7a266179ab829c5
Vsein <vseinssj@gmail.com>
2022-11-14 23:27:47 +0300

    Clean code for logo generation

--- src/pages/Logo/index.js
+++ src/pages/Logo/index.js
@@ -1,9 +1,4 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route } from 'react-router-dom';
-// import Habits from './Habits';
-import { useParams, useNavigate, Outlet, useLocation } from 'react-router-dom';
-import Icon from '@mdi/react';
-import { mdiPlus } from '@mdi/js';
+import React, { useEffect } from 'react';
 
 export default function HabitsPage() {
   useEffect(() => {
@@ -22,63 +17,35 @@ function randomRange(start, end) {
 }
 
 function Line(gapStart, gapLength, inc, i, start, len=14) {
-  const dateEnd = new Date();
-  const dateStart = new Date(new Date().setFullYear(dateEnd.getFullYear() - 1));
   let data = [];
-  // const periods = [gapStart + i * inc, gapLength / 7, len - gapStart - gapLength - i * inc];
   const periods = [gapStart + i * inc, gapLength, len - gapStart - gapLength - i * inc];
   data = data.concat(LineGap(start, periods[0]));
-  // data = data.concat(LineActive(start + periods[0], periods[1], 7));
-  // data = data.concat(LineActiveRandom(start + data.length, periods[1]));
   data = data.concat(LineActiveRandom(start + data.length, periods[1]));
   data = data.concat(LineGap(start + data.length, periods[2]));
   return data;
 }
 
-function LineActive(start, len=14, height=1, width=1) {
-  const dateEnd = new Date();
-  const dateStart = new Date(new Date().setFullYear(dateEnd.getFullYear() - 1));
-  const dataActive = Array.from(new Array(len)).map((_, index) => ({
-    date: new Date(
-      new Date().setDate(dateStart.getDate() + index + start),
-    ),
-    value: randomRange(25, 100),
-    height,
-    width,
-  }));
-  return dataActive;
-}
-
 function LineActiveStraight(start, len=14, width=1) {
+  // returns a cell of height len. If len === 1, returns a square cell
   const dateEnd = new Date();
   const dateStart = new Date(new Date().setFullYear(dateEnd.getFullYear() - 1));
-  const dataActive = Array.from(new Array(1)).map((_, index) => ({
+  return {
     date: new Date(
-      new Date().setDate(dateStart.getDate() + index + start),
+      new Date().setDate(dateStart.getDate() + start),
     ),
     value: randomRange(25, 100),
     height: len,
     width,
-  }));
-  return dataActive;
+  };
 }
 
-function LineActiveRandom(start, len=14, height=1, width=1) {
-  const dateEnd = new Date();
-  const dateStart = new Date(new Date().setFullYear(dateEnd.getFullYear() - 1));
-  let dataActive = [];
+function LineActiveRandom(start, len=14, height=1, width=1, min=Infinity) {
+  const dataActive = [];
   let cnt = 0;
   for (let i = 0; i < len;) {
-    const curLen = randomRange(1, Math.min(5, len - i));
+    const curLen = randomRange(1, Math.min(5, len - i, min));
+    dataActive.push(LineActiveStraight(cnt + start, curLen));
     i += curLen;
-    dataActive.push({
-      date: new Date(
-        new Date().setDate(dateStart.getDate() + cnt + start),
-      ),
-      value: randomRange(60, 100),
-      height: curLen,
-      width,
-    });
     cnt++;
   }
   return dataActive;
@@ -98,26 +65,14 @@ function LineGap(start, len=14) {
   return dataActive;
 }
 
-function Habits() {
-  const dateEnd = new Date();
-  const dateStart = new Date(new Date().setFullYear(dateEnd.getFullYear() - 1));
-  const diffTime = Math.abs(dateStart - dateEnd);
-  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-
+function Logo() {
   let data = [];
 
   const len = 14;
 
-  // data = data.concat(LineActiveRandom(data.length, 14));
-  // data = data.concat(LineActiveRandom(data.length, 14));
-  // data = data.concat(LineActiveRandom(data.length, 14));
-  // data = data.concat(Line(data.length, 14));
-  // data = data.concat(Line(data.length, 14));
-  // data = data.concat(Line(data.length, 14));
   data = data.concat(Line(2, 10, 1, 0, data.length, len));
   data = data.concat(Line(1, 12, 1, 0, data.length, len));
   data = data.concat(Line(0, 14, 1, 0, data.length, len));
-  // data = data.concat(LineActive(data.length, 1, 14));
 
   for (let i = 0; i < 6; i++) {
     data = data.concat(Line(1, 7, 1, i, data.length, len));
@@ -126,15 +81,16 @@ function Habits() {
   data = data.concat(Line(0, 14, 1, 0, data.length, len));
   data = data.concat(Line(1, 12, 1, 0, data.length, len));
   data = data.concat(Line(2, 10, 1, 0, data.length, len));
-  // data = data.concat(LineActiveRandom(data.length, 14));
-  // data = data.concat(LineActiveRandom(data.length, 14));
-  // data = data.concat(LineActiveRandom(data.length, 14));
-  // data = data.concat(LineActive(data.length, 1, 14));
-  // data = data.concat(LineActive(data.length, 1, 14));
-  // data = data.concat(LineActive(data.length, 1, 14));
-  // data = data.concat(LineActive(data.length, len));
-  // data = data.concat(LineActive(data.length, len));
-  // data = data.concat(LineActive(data.length, len));
+  return data;
+}
+
+function Habits() {
+  const dateEnd = new Date();
+  const dateStart = new Date(new Date().setFullYear(dateEnd.getFullYear() - 1));
+  const diffTime = Math.abs(dateStart - dateEnd);
+  const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+
+  let data = Logo();
 
   return (
     <main className="logonh">
@@ -147,7 +103,6 @@ function Habits() {
       <div className="logonh-container">
         <h1 className="logo neohabit" />
       </div>
-      <Outlet />
     </main>
   );
 }
@@ -167,7 +122,6 @@ function Cell({ color, date, value, height, width }) {
 }
 
 function Timeline({ dateStart, dateEnd, data, colorFunc }) {
-  console.log(data.length);
   const cells = Array.from(new Array(data.length));
 
   const min = Math.min(0, ...data.map((d) => d.value));
