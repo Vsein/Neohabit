cfa7fb8e75f8c66cb2bf8ac84ab091dccbbf52bb
Vsein <vseinssj@gmail.com>
2023-07-20 05:41:56 +0300

    Button for project creation on overview

--- src/pages/Overview/index.jsx
+++ src/pages/Overview/index.jsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import Icon from '@mdi/react';
+import { useDispatch } from 'react-redux';
+import { mdiPlus } from '@mdi/js';
 import { subMonths, startOfDay } from 'date-fns';
 import PFP from '../../components/ProfilePicture';
 import Overview from '../../components/Overview';
 import useTitle from '../../hooks/useTitle';
+import { changeTo, open } from '../../state/features/projectOverlay/projectOverlaySlice';
 
 export default function Dashboard() {
   useTitle('Dashboard | Neohabit');
+  const dispatch = useDispatch();
   const dateEnd = startOfDay(new Date());
   const dateStart = subMonths(dateEnd, 1);
+
+  const openOverlay = () => {
+    dispatch(open());
+    dispatch(changeTo(''));
+  };
 
   return (
     <div id="content-overview">
@@ -18,6 +27,10 @@ export default function Dashboard() {
         <div className="overview-container">
           <Overview dateStart={dateStart} dateEnd={dateEnd} />
         </div>
+        <button className="overview-project-add" onClick={openOverlay} >
+          <Icon className="icon small" path={mdiPlus} />
+          <p>Add project</p>
+        </button>
       </div>
     </div>
   );
