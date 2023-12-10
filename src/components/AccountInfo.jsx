0c2891cb4f06ea8353e4793febee60a87e5ee819
Vsein <vseinssj@gmail.com>
2023-12-10 19:22:43 +0300

    Use neohabit mini logos through css, not js

--- src/components/AccountInfo.jsx
+++ src/components/AccountInfo.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import MiniLogoDark from '../logos/neohabit-mini-logo-dark2.png';
-import MiniLogo from '../logos/neohabit-mini-logo.png';
 import { useGetSelfQuery, useGetSettingsQuery } from '../state/services/settings';
 
 export default function AccountInfo() {
@@ -11,11 +9,7 @@ export default function AccountInfo() {
     <></>
   ) : (
     <div className="account-info">
-      <img
-        src={settings.data.prefer_dark ? MiniLogoDark : MiniLogo}
-        alt="profile pic"
-        className="pfp small"
-      />
+      <img alt="profile pic" className="pfp small" />
       <p>{`@${self.data.username}`}</p>
       <p className="account-username">{self.data.email}</p>
     </div>
--- src/components/ProfilePicture.jsx
+++ src/components/ProfilePicture.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import MiniLogoDark from '../logos/neohabit-mini-logo-dark2.png';
-import MiniLogo from '../logos/neohabit-mini-logo.png';
 import { useGetSettingsQuery } from '../state/services/settings';
 
 export default function PFP({ type }) {
@@ -10,11 +8,7 @@ export default function PFP({ type }) {
     <></>
   ) : (
     <a tabIndex="0" className="centering">
-      <img
-        src={settings.data.prefer_dark ? MiniLogoDark : MiniLogo}
-        alt="profile pic"
-        className={`pfp ${type}`}
-      />
+      <img alt="profile pic" className={`pfp ${type}`} />
     </a>
   );
 }
