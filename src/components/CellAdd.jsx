acf614a77cbf76bcc2f8951de2148eeff63b4536
Vsein <vseinssj@gmail.com>
2023-12-21 20:06:31 +0300

    Move both PointForm and TargetForm into CellAdd

--- src/components/CellAdd.jsx
+++ src/components/CellAdd.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import CellAddPointForm from './CellAddPointForm';
-import CellAddTargetForm from './CellAddTargetForm';
+import { formatISO } from 'date-fns';
+import { Form } from 'react-final-form';
+import { Icon } from '@mdi/react';
+import { mdiPlus } from '@mdi/js';
 import { useUpdateHeatmapMutation } from '../state/services/heatmap';
 import { close } from '../state/features/cellAdd/cellAddSlice';
+import { DateField, ActionsField, PeriodField } from './CellAddFields';
 
 export default function CellAdd() {
   const dispatch = useDispatch();
@@ -42,5 +45,65 @@ export default function CellAdd() {
       <h3>Add completed actions</h3>
       <CellAddPointForm onSubmit={onSubmit} />
     </div>
+  );
+}
+
+function CellAddPointForm({ onSubmit }) {
+  return (
+    <Form
+      initialValues={{
+        date: formatISO(new Date(), { representation: 'date' }),
+        value: 1,
+      }}
+      onSubmit={onSubmit}
+      render={({ handleSubmit, form, submitting, pristine, values }) => (
+        <form
+          onSubmit={async (e) => {
+            await handleSubmit(e);
+            form.reset();
+          }}
+          className="habit-form"
+        >
+          <DateField />
+          <ActionsField />
+          <button className="habit-button" type="submit" disabled={submitting}>
+            <Icon path={mdiPlus} className="icon small" />
+          </button>
+        </form>
+      )}
+    />
+  );
+}
+
+function CellAddTargetForm({ onSubmit }) {
+  return (
+    <Form
+      initialValues={{
+        date: formatISO(new Date(), { representation: 'date' }),
+        value: 1,
+        period: 1,
+        is_target: true,
+      }}
+      onSubmit={onSubmit}
+      render={({ handleSubmit, form, submitting, pristine, values }) => (
+        <form
+          onSubmit={async (e) => {
+            await handleSubmit(e);
+          }}
+          className="habit-form target"
+        >
+          <span>Starting from </span>
+          <DateField />
+          <span>, aim to do</span>
+          <ActionsField />
+          <span> action(s) in </span>
+          <PeriodField />
+          <span>day(s)</span>
+          <button className="habit-button right" type="submit" disabled={submitting}>
+            <Icon path={mdiPlus} className="icon small" />
+          </button>
+        </form>
+      )}
+    />
   );
 }
---
+++ src/components/CellAddFields.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import Field from './FieldWrapper';
+import { requiredValidator, numberBoundsValidator, composeValidators } from '../utils/validators';
+
+function DateField() {
+  return (
+    <div className="habit-form-date">
+      <label htmlFor="date-name">
+        <Field
+          name="date"
+          component="input"
+          type="date"
+          placeholder="Change habit name"
+          // max="<?= date('Y-m-d'); ?>"
+          rows="1"
+          className="habit-form-input"
+          required
+        />
+      </label>
+    </div>
+  );
+}
+
+function ActionsField() {
+  return (
+    <Field
+      name="value"
+      validate={composeValidators(requiredValidator, numberBoundsValidator(1, 999))}
+    >
+      {({ input, meta }) => (
+        <div
+          className={`habit-form-counter
+${input?.value <= 0 || input?.value >= 1000 ? 'error' : ''}
+                `}
+        >
+          <label htmlFor="date-name"> </label>
+          <input {...input} className="habit-form-input" type="number" placeholder="1" />
+        </div>
+      )}
+    </Field>
+  );
+}
+
+function PeriodField() {
+  return (
+    <Field
+      name="period"
+      validate={composeValidators(requiredValidator, numberBoundsValidator(1, 36500))}
+    >
+      {({ input, meta }) => (
+        <div
+          className={`habit-form-counter
+${input?.value <= 0 || input?.value >= 36500 ? 'error' : ''}
+                `}
+        >
+          <label htmlFor="date-name"> </label>
+          <input {...input} className="habit-form-input" type="number" placeholder="1" />
+        </div>
+      )}
+    </Field>
+  );
+}
+
+export { DateField, ActionsField, PeriodField };
