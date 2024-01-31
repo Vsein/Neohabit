bdadeaba45153fed8af9b28eed32b53662e6a74f
Vsein <vseinssj@gmail.com>
2024-01-31 19:55:13 +0300

    Move some of the projectlist logic to the page-specific jsx

--- src/components/Projectlist.jsx
+++ src/components/Projectlist.jsx
@@ -1,84 +1,25 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { Icon } from '@mdi/react';
-import { mdiPlus } from '@mdi/js';
 import { useGetHabitsQuery } from '../state/services/habit';
 import { useGetProjectsQuery } from '../state/services/project';
-import { useGetSettingsQuery } from '../state/services/settings';
-import { changeTo } from '../state/features/overlay/overlaySlice';
-import useDatePeriod, { getAdaptivePeriodLength } from '../hooks/useDatePeriod';
-import useWindowDimensions from '../hooks/useWindowDimensions';
 import useLoaded from '../hooks/useLoaded';
-import useKeyPress from '../hooks/useKeyPress';
 import useDefaultProject from '../hooks/useDefaultProject';
 import { ProjectWrapper } from './Project';
-import { DatePeriodPicker } from './DatePickers';
 
-export default function Projectlist() {
+export default function Projectlist({
+  datePeriodLength, mobile, dateStart, dateEnd, subPeriod, addPeriod
+}) {
   const [loaded] = useLoaded();
   const projects = useGetProjectsQuery();
   const habits = useGetHabitsQuery();
-  const settings = useGetSettingsQuery();
-  const vertical = false;
-
-  const dispatch = useDispatch();
-  const openOverlay = () => {
-    dispatch(changeTo({ projectID: '', type: 'project' }));
-  };
-
   const [defaultProject] = useDefaultProject();
 
-  const { width } = useWindowDimensions();
-  const { adaptiveDatePeriodLength, mobile } = getAdaptivePeriodLength(width);
-  const datePeriodLength =
-    settings.data?.overview_adaptive ?? true
-      ? Math.min(
-          adaptiveDatePeriodLength,
-          settings.data?.overview_apply_limit ?? true
-            ? settings.data?.overview_duration_limit ?? 32
-            : Infinity,
-        )
-      : settings.data?.overview_duration ?? 32;
-
-  const [
-    dateEnd,
-    setDateEnd,
-    dateStart,
-    setDateStart,
-    { subMonth, addMonth, subYear, addYear, subPeriod, addPeriod, setToPast, setToFuture, reset },
-  ] = useDatePeriod(datePeriodLength, true);
-
-  if (!loaded || projects.isFetching || habits.isFetching || settings.isFetching) {
+  if (!loaded || projects.isFetching || habits.isFetching) {
     return <div className="loader" />;
   }
 
   return (
-    <>
-      <div className="contentlist-controls">
-        <div className="overview-centering" style={{ width: 'max-content' }}>
-          <button
-            className={`overview-habit-add standalone topbar ${vertical ? 'vertical' : ''}`}
-            onClick={openOverlay}
-            title="Add a new project"
-          >
-            <p>New project</p>
-          </button>
-        </div>
-        <DatePeriodPicker
-          dateStart={dateStart}
-          setDateStart={setDateStart}
-          dateEnd={dateEnd}
-          setDateEnd={setDateEnd}
-          subPeriod={subPeriod}
-          addPeriod={addPeriod}
-          setToPast={setToPast}
-          reset={reset}
-          setToFuture={setToFuture}
-        />
-      </div>
-      <div className="contentlist">
-        {projects.data &&
+    <div className="contentlist">
+      {projects.data &&
           projects.data.map((project, i) => (
             <ProjectWrapper
               key={i}
@@ -91,20 +32,19 @@ export default function Projectlist() {
               globalAddPeriod={addPeriod}
             />
           ))}
-        {defaultProject.habits.length ? (
-          <ProjectWrapper
-            project={defaultProject}
-            datePeriodLength={datePeriodLength}
-            mobile={mobile}
-            globalDateStart={dateStart}
-            globalDateEnd={dateEnd}
-            subPeriod={subPeriod}
-            addPeriod={addPeriod}
-          />
-        ) : (
-          <></>
-        )}
-      </div>
-    </>
+      {defaultProject.habits.length ? (
+        <ProjectWrapper
+          project={defaultProject}
+          datePeriodLength={datePeriodLength}
+          mobile={mobile}
+          globalDateStart={dateStart}
+          globalDateEnd={dateEnd}
+          subPeriod={subPeriod}
+          addPeriod={addPeriod}
+        />
+      ) : (
+        <></>
+      )}
+    </div>
   );
 }
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,11 +1,79 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { Icon } from '@mdi/react';
+import { mdiPlus } from '@mdi/js';
+import { useGetSettingsQuery } from '../state/services/settings';
+import { changeTo } from '../state/features/overlay/overlaySlice';
+import useDatePeriod, { getAdaptivePeriodLength } from '../hooks/useDatePeriod';
+import useWindowDimensions from '../hooks/useWindowDimensions';
+import useKeyPress from '../hooks/useKeyPress';
+import { DatePeriodPicker } from '../components/DatePickers';
 import Projectlist from '../components/Projectlist';
 import useTitle from '../hooks/useTitle';
 
 export default function ProjectsPage() {
   useTitle('Projects | Neohabit');
+  const settings = useGetSettingsQuery();
+  const vertical = false;
+
+  const dispatch = useDispatch();
+  const openOverlay = () => {
+    dispatch(changeTo({ projectID: '', type: 'project' }));
+  };
+
+  const { width } = useWindowDimensions();
+  const { adaptiveDatePeriodLength, mobile } = getAdaptivePeriodLength(width);
+  const datePeriodLength =
+    settings.data?.overview_adaptive ?? true
+      ? Math.min(
+          adaptiveDatePeriodLength,
+          settings.data?.overview_apply_limit ?? true
+            ? settings.data?.overview_duration_limit ?? 32
+            : Infinity,
+        )
+      : settings.data?.overview_duration ?? 32;
+
+  const [
+    dateEnd,
+    setDateEnd,
+    dateStart,
+    setDateStart,
+    { subMonth, addMonth, subYear, addYear, subPeriod, addPeriod, setToPast, setToFuture, reset },
+  ] = useDatePeriod(datePeriodLength, true);
 
   return (
-    <Projectlist />
+    <>
+      <div className="contentlist-controls">
+        <div className="overview-centering" style={{ width: 'max-content' }}>
+          <button
+            className={`overview-habit-add standalone topbar ${vertical ? 'vertical' : ''}`}
+            onClick={openOverlay}
+            title="Add a new project"
+          >
+            <p>New project</p>
+          </button>
+        </div>
+        <DatePeriodPicker
+          dateStart={dateStart}
+          setDateStart={setDateStart}
+          dateEnd={dateEnd}
+          setDateEnd={setDateEnd}
+          subPeriod={subPeriod}
+          addPeriod={addPeriod}
+          setToPast={setToPast}
+          reset={reset}
+          setToFuture={setToFuture}
+        />
+      </div>
+      <Projectlist
+        datePeriodLength={datePeriodLength}
+        mobile={mobile}
+        dateStart={dateStart}
+        dateEnd={dateEnd}
+        subPeriod={subPeriod}
+        addPeriod={addPeriod}
+      />
+    </>
   );
 }
