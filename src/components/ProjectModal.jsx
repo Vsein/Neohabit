cd1a0a2d7eadfae64c101473112715b10b136058
Vsein <vseinssj@gmail.com>
2024-02-01 03:04:14 +0300

    Lint + Fix when there are no habits in the project modal

--- src/components/ProjectModal.jsx
+++ src/components/ProjectModal.jsx
@@ -66,128 +66,133 @@ export default function ProjectModal({ projectID, isActive, closeOverlay }) {
     return res !== -1;
   };
 
-  return loaded && (
-    <Form
-      initialValues={{
-        name: project?.name,
-        description: project?.description,
-        color: project?.color,
-      }}
-      onSubmit={onSubmit}
-      render={({ handleSubmit, form, submitting, pristine, values }) => (
-        <form
-          onSubmit={async (e) => {
-            await handleSubmit(e);
-          }}
-          className="modal modal-active"
-          onClick={(e) => e.stopPropagation()}
-          onMouseDown={(e) => e.stopPropagation()}
-        >
-          <div className="modal-header">
-            {projectID ? (
-              <div className="tag-wrapper">
-                Project:
-                <NavLink
-                  className="tag"
-                  onClick={closeOverlay}
-                  to={`../project/${project?._id}`}
-                  title={project.name}
-                >
-                  <HabitTag habit={project} />
-                </NavLink>
+  return (
+    loaded && (
+      <Form
+        initialValues={{
+          name: project?.name,
+          description: project?.description,
+          color: project?.color,
+        }}
+        onSubmit={onSubmit}
+        render={({ handleSubmit, form, submitting, pristine, values }) => (
+          <form
+            onSubmit={async (e) => {
+              await handleSubmit(e);
+            }}
+            className="modal modal-active"
+            onClick={(e) => e.stopPropagation()}
+            onMouseDown={(e) => e.stopPropagation()}
+          >
+            <div className="modal-header">
+              {projectID ? (
+                <div className="tag-wrapper">
+                  Project:
+                  <NavLink
+                    className="tag"
+                    onClick={closeOverlay}
+                    to={`../project/${project?._id}`}
+                    title={project.name}
+                  >
+                    <HabitTag habit={project} />
+                  </NavLink>
+                </div>
+              ) : (
+                <div className="tag-wrapper" style={{ height: '100%' }}>
+                  New project
+                </div>
+              )}
+              <button className="icon small" onClick={closeOverlay} type="button" title="Close [C]">
+                <Icon path={mdiClose} />
+              </button>
+            </div>
+            <div className="modal-details-block" style={{ height: 'min-content' }}>
+              <NameField type="project" />
+            </div>
+            <div className="modal-header habit-mode">
+              <p>Habits:</p>
+              <div
+                className={`form-chooser add form-habits-toggle ${menuOpened ? 'active' : ''}`}
+                onClick={toggleMenu}
+                type="button"
+                title="Close [C]"
+              >
+                <Icon className="icon small" path={mdiPlus} />
+                <p>Add existing habits</p>
               </div>
-            ) : (
-              <div className="tag-wrapper" style={{ height: '100%' }}>
-                New project
+              <ul
+                className={`form-habits-container ${menuOpened ? 'active' : ''}`}
+                onClick={(e) => e.stopPropagation()}
+              >
+                {habits.data.map(
+                  (habit, i) =>
+                    !isOneOfHabits(habit._id) && (
+                      <div
+                        className="form-chooser"
+                        key={i}
+                        onClick={() => {
+                          if (!isOneOfHabits(habit._id)) {
+                            setProjectHabitList([...projectHabitList, habit._id]);
+                          }
+                        }}
+                      >
+                        <HabitTag habit={habit} />
+                      </div>
+                    ),
+                )}
+              </ul>
+            </div>
+            {!!projectHabitList?.length && (
+              <div
+                className="modal-details-block"
+                style={{
+                  height: 'auto',
+                  maxHeight: '200px',
+                  overflowY: 'scroll',
+                  backgroundColor: 'var(--support-background-color)',
+                  marginTop: '-10px',
+                }}
+              >
+                <div className="form-habits">
+                  {projectHabitList.map((habitID, i) => {
+                    const habit = habits.data.find((habito) => habito._id === habitID);
+                    return (
+                      <div
+                        className="form-chooser"
+                        key={i}
+                        onClick={() => {
+                          if (!isOneOfHabits(habit._id)) {
+                            setProjectHabitList([...projectHabitList, habit._id]);
+                          } else {
+                            setProjectHabitList(
+                              projectHabitList.filter((habitIDo) => habitIDo !== habit._id),
+                            );
+                          }
+                        }}
+                      >
+                        <HabitTagToDelete habit={habit} />
+                      </div>
+                    );
+                  })}
+                </div>
               </div>
             )}
-            <button className="icon small" onClick={closeOverlay} type="button" title="Close [C]">
-              <Icon path={mdiClose} />
-            </button>
-          </div>
-          <div className="modal-details-block" style={{ height: 'min-content' }}>
-            <NameField type="project" />
-          </div>
-          <div className="modal-header habit-mode" style={{ marginBottom: '-10px' }}>
-            <p>Habits:</p>
-            <div
-              className={`form-chooser add form-habits-toggle ${menuOpened ? 'active' : ''}`}
-              onClick={toggleMenu}
-              type="button"
-              title="Close [C]"
-            >
-              <Icon className="icon small" path={mdiPlus} />
-              <p>Add existing habits</p>
+            <div className="modal-details-project-wrapper">
+              <div className="modal-details-block description-area">
+                <DescriptionField rows="9" />
+              </div>
+              <div className="modal-details-block color-area">
+                <ColorPicker />
+              </div>
             </div>
-            <ul
-              className={`form-habits-container ${menuOpened ? 'active' : ''}`}
-              onClick={(e) => e.stopPropagation()}
-            >
-              {habits.data.map(
-                (habit, i) =>
-                  !isOneOfHabits(habit._id) && (
-                    <div
-                      className="form-chooser"
-                      key={i}
-                      onClick={() => {
-                        if (!isOneOfHabits(habit._id)) {
-                          setProjectHabitList([...projectHabitList, habit._id]);
-                        }
-                      }}
-                    >
-                      <HabitTag habit={habit} />
-                    </div>
-                  ),
-              )}
-            </ul>
-          </div>
-          <div
-            className="modal-details-block"
-            style={{
-              height: 'auto',
-              maxHeight: '200px',
-              overflowY: 'scroll',
-              backgroundColor: 'var(--support-background-color)',
-            }}
-          >
-            <div className="form-habits">
-              {projectHabitList.map((habitID, i) => {
-                const habit = habits.data.find((habito) => habito._id === habitID);
-                return (
-                  <div
-                    className="form-chooser"
-                    key={i}
-                    onClick={() => {
-                      if (!isOneOfHabits(habit._id)) {
-                        setProjectHabitList([...projectHabitList, habit._id]);
-                      } else {
-                        setProjectHabitList(
-                          projectHabitList.filter((habitIDo) => habitIDo !== habit._id),
-                        );
-                      }
-                    }}
-                  >
-                    <HabitTagToDelete habit={habit} />
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-          <div className="modal-details-project-wrapper">
-            <div className="modal-details-block description-area">
-              <DescriptionField rows="9" />
-            </div>
-            <div className="modal-details-block color-area">
-              <ColorPicker />
-            </div>
-          </div>
-          <ModalButtons
-            disabled={submitting || !values?.name}
-            isNew={!projectID}
-            type="project"
-          />
-        </form>
-      )}
-    />
+            <ModalButtons
+              disabled={submitting || !values?.name}
+              isNew={!projectID}
+              type="project"
+            />
+          </form>
+        )}
+      />
+    )
   );
 }
