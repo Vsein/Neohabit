0aa8dc17758e93ac2799a19cc919fa8116355305
Vsein <vseinssj@gmail.com>
2024-02-06 14:54:34 +0300

    Remove the isFetching check from sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -37,8 +37,6 @@ export default function Sidebar({ hidden }) {
 
   const [defaultProject] = useDefaultProject();
 
-  if (projects.isFetching || habits.isFetching) return <></>;
-
   return (
     <aside className={hidden ? 'sidebar sidebar-hidden' : 'sidebar'}>
       <ul className="navigation">
