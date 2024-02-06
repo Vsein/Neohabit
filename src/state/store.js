67c67d02ece693f637f75114cf3db34345be47d9
Vsein <vseinssj@gmail.com>
2024-02-06 18:45:52 +0300

    Redux-persist for a pwa version

--- src/state/store.js
+++ src/state/store.js
@@ -1,11 +1,23 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import localForage from 'localforage';
 import api from './services/api';
 import authApi from './services/auth';
 import rtkQueryErrorLogger from './services/errorHandler';
 import overlayReducer from './features/overlay/overlaySlice';
 import cellAddReducer from './features/cellAdd/cellAddSlice';
 import cellTipReducer from './features/cellTip/cellTipSlice';
+import isPWA from '../utils/pwa';
 
 const combinedReducer = combineReducers({
   [api.reducerPath]: api.reducer,
@@ -24,16 +36,32 @@ const rootReducer = (state, action) => {
   return combinedReducer(state, action);
 };
 
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage: localForage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store = configureStore({
-  reducer: rootReducer,
+  reducer: isPWA() ? persistedReducer : rootReducer,
   // Adding the api middleware enables caching, invalidation, polling,
   // and other useful features of `rtk-query`.
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }).concat([rtkQueryErrorLogger, api.middleware, authApi.middleware]),
+    getDefaultMiddleware({
+      serializableCheck: isPWA()
+        ? {
+            ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+          }
+        : false,
+    }).concat([rtkQueryErrorLogger, api.middleware, authApi.middleware]),
 });
+
+const persistor = persistStore(store);
 
 // optional, but required for refetchOnFocus/refetchOnReconnect behaviors
 // see `setupListeners` docs - takes an optional callback as the 2nd arg for customization
 setupListeners(store.dispatch);
 
-export default store;
+export { store, persistor };
