c97e0082531060ac6e9d3fcdcf6f47a10ea24f05
Vsein <vseinssj@gmail.com>
2024-12-15 06:17:06 +0400

    Fix the projects not draggin and droppin

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -108,7 +108,7 @@ export default function Project({
     const data = e.dataTransfer.getData("text");
     const draggedProject = document.getElementById(data);
 
-    if (!draggedProject || !draggedProject.classList.contains('.overview-centering')) {
+    if (!draggedProject || !draggedProject.classList.contains('overview-centering')) {
       return;
     };
 
