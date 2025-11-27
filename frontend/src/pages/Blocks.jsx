c60315b72c5c4f5b1232c13028c44f42e3851047
Vsein <vseinssj@gmail.com>
2025-11-27 09:43:49 +0400

    Stop pretending that "I" am "We" :D

--- frontend/src/pages/Blocks.jsx
+++ frontend/src/pages/Blocks.jsx
@@ -12,20 +12,20 @@ export default function NotFound() {
         <h3>A sneak peek</h3>
       </div>
       <p className="back-ref" style={{ width: 'min(100%, 800px)' }}>
-        Our ultimate vision for Neohabit is to have a big public base of habits, which you would be
-        able to explore and pick the ones you would like to build. We aim to make them easily
-        importable to your account, with preset progressions and goals.
+        My ultimate vision for Neohabit is to have a public, community-driven base of habits,
+        which you would be able to explore and pick the ones you would like to build. I&apos;ll aim to
+        make them easily importable to your account, with preset progressions and goals.
       </p>
       <p className="back-ref" style={{ width: 'min(100%, 800px)' }}>
-        We belive it would prove to be incredibly useful, because sometimes there are things which
+        I belive it would prove to be incredibly useful, because sometimes there are things which
         need to be followed to preserve our health and edge, like optimizing melatonin production
         for better sleep (see Andrew Huberman&apos;s podcast on the topic). Those sets of habits are
         basically building blocks of our lives, hence the name Blocks.
       </p>
       <p className="back-ref" style={{ width: 'min(100%, 800px)' }}>
-        Since our implementation of habit-building is easily customizable, very forgiving in terms
-        of time consumption, and ridden of self-enforced unrealistic schedules, we belive it to be a
-        perfect foundation for a public base of habits.
+        Since my implementation of habit-building is easily customizable, very forgiving in terms
+        of time consumption, and ridden of self-enforced unrealistic schedules, I believe it to be a
+        good foundation for a public base of habits.
       </p>
       <p className="back-ref">
         <Link onClick={() => navigate(-1)}>Go back</Link>
