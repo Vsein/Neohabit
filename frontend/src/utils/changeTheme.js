37c8aea6e93396d19a76d6345ab01cb4e00b6955
Vsein <61903799+Vsein@users.noreply.github.com>
2025-12-28 04:44:41 +0400

    Logos: remove attempt at updating favicon on theme change

--- frontend/src/utils/changeTheme.js
+++ frontend/src/utils/changeTheme.js
@@ -1,7 +1,5 @@
 export default function changeTheme(newTheme) {
   const root = document.documentElement;
-  const link = document.querySelector("link[rel~='icon']");
-  link.href = newTheme === 'dark' ? '/favicon-dark2.ico' : '/favicon.ico';
   document
     .querySelector('meta[name="theme-color"]')
     .setAttribute('content', newTheme === 'dark' ? '#1c2128' : '#00c4cd');
