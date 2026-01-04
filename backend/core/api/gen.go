a1ebdab91cac3c74a682b8ec2f6433380234598a
Vsein <61903799+Vsein@users.noreply.github.com>
2026-01-04 05:03:33 +0400

    OpenAPI: add config.yaml with a name-normalizer option
    Quite neatly transforms id into ID

--- backend/core/api/gen.go
+++ backend/core/api/gen.go
@@ -1,6 +1,5 @@
 package api
 
-//go:generate go tool oapi-codegen -o ../internal/input/http/gen/types.go -generate types -package gen openapi.yaml
-//go:generate go tool oapi-codegen -o ../internal/input/http/gen/server.go -generate chi-server,strict-server -package gen openapi.yaml
-//go:generate go tool oapi-codegen -o ../internal/input/http/gen/spec.go -generate spec -package gen openapi.yaml
-
+//go:generate go tool oapi-codegen -o ../internal/input/http/gen/types.go --config=config.yaml -generate types openapi.yaml
+//go:generate go tool oapi-codegen -o ../internal/input/http/gen/server.go --config=config.yaml -generate chi-server,strict-server openapi.yaml
+//go:generate go tool oapi-codegen -o ../internal/input/http/gen/spec.go --config=config.yaml -generate spec openapi.yaml
