3694656ed42b420bc47727044c30ce4cd24329ab
Vsein <61903799+Vsein@users.noreply.github.com>
2026-01-14 12:39:32 +0400

    Add optimistic updates for new habitData creation

--- frontend/src/state/services/habitData.js
+++ frontend/src/state/services/habitData.js
@@ -1,4 +1,8 @@
 import api from './api';
+import { habitApi } from './habit';
+import { projectApi } from './project';
+import { findFirstIndexBsearch } from '../../utils/binarySearch';
+import { areAscending } from '../../utils/dates';
 
 export const habitDataApi = api.injectEndpoints({
   endpoints: (builder) => ({
@@ -8,6 +12,39 @@ export const habitDataApi = api.injectEndpoints({
         body: values,
         method: 'POST',
       }),
+      async onQueryStarted({ habitID, values }, { dispatch }) {
+        const isAfterDataPoint = (dataPoint) => areAscending(values.date, dataPoint.date);
+        const addDataPointToHabit = (habit) => {
+          if (habit) {
+            const i = findFirstIndexBsearch(habit.data, isAfterDataPoint);
+            if (i !== -1) {
+              habit.data.splice(i, 0, values);
+            } else {
+              habit.data.push(values);
+            }
+          }
+        };
+
+        dispatch(
+          projectApi.util.updateQueryData('getProjects', undefined, (draft) => {
+            draft.forEach((project) => {
+              addDataPointToHabit(project.habits.find((h) => h.id === habitID));
+            });
+          }),
+        );
+
+        dispatch(
+          habitApi.util.updateQueryData('getHabits', undefined, (draft) => {
+            addDataPointToHabit(draft.find((h) => h.id === habitID));
+          }),
+        );
+
+        dispatch(
+          habitApi.util.updateQueryData('getHabitsOutsideProjects', undefined, (draft) => {
+            addDataPointToHabit(draft.find((h) => h.id === habitID));
+          }),
+        );
+      },
     }),
   }),
 });
