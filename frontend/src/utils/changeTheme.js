576f921135e117b1958f871aa5dc30678b272bce
Vsein <61903799+Vsein@users.noreply.github.com>
2026-01-18 18:19:59 +0400

    Revert "Logos: remove attempt at updating favicon on theme change"
    This reverts commit 37c8aea6e93396d19a76d6345ab01cb4e00b6955.

--- frontend/src/utils/changeTheme.js
+++ frontend/src/utils/changeTheme.js
@@ -1,8 +1,10 @@
 export default function changeTheme(newTheme) {
   const root = document.documentElement;
+  const link = document.querySelector("link[rel~='icon']");
+  link.href = newTheme === 'dark' ? '/favicon-dark.ico' : '/favicon.ico';
   document
     .querySelector('meta[name="theme-color"]')
     .setAttribute('content', newTheme === 'dark' ? '#1c2128' : '#00c4cd');
   root.className = newTheme;
   localStorage.setItem('theme', newTheme);
-};
+}
