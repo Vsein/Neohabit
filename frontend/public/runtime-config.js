e1c5dda45819bf0d3791b59a13bd1c9c9cfc2e94
Vsein <61903799+Vsein@users.noreply.github.com>
2026-02-02 06:37:00 +0400

    fix: add clarifying word

--- frontend/public/runtime-config.js
+++ frontend/public/runtime-config.js
@@ -1,5 +1,5 @@
 // config that isn't processed by the bundler
-// NOTE: for `npm start` testing, not docker
+// NOTE: for direct `npm start` testing, not docker
 window.APP_CONFIG = {
   API_URL: 'http://localhost:9000/',
   DISABLE_SIGNUP: false,
