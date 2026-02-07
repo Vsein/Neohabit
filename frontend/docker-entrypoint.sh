3e09ec7c8c0907dd5771347636bc1671d5dd15ec
Vsein <61903799+Vsein@users.noreply.github.com>
2026-02-08 00:20:17 +0400

    deploy: better defaults for docker-compose.yaml, .env.example and nginx

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -5,7 +5,11 @@ envsubst \
   < /usr/share/nginx/html/runtime-config.template.js \
   > /usr/share/nginx/html/runtime-config.js
 
-envsubst '$BACKEND_PORT' \
+export DOMAIN="${DOMAIN:-localhost}"
+export BACKEND_HOST="${BACKEND_HOST:-backend}"
+export BACKEND_PORT="${BACKEND_PORT:-9000}"
+
+envsubst '$DOMAIN $BACKEND_HOST $BACKEND_PORT' \
   < /etc/nginx/conf.d/default.conf.template \
   > /etc/nginx/conf.d/default.conf
 
