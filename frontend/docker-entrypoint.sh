f21036b06efce83b8a078b916a50db65e8d4d381
Vsein <61903799+Vsein@users.noreply.github.com>
2026-02-10 20:35:54 +0400

    deploy: properly set up caddy; finally working with localhost

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -e
 
-envsubst \
-  < /usr/share/nginx/html/runtime-config.template.js \
-  > /usr/share/nginx/html/runtime-config.js
+sed -i \
+  "s|\$DISABLE_SIGNUP|${DISABLE_SIGNUP}|g; \
+   s|\$STRICT_USER_FIELDS|${STRICT_USER_FIELDS}|g; \
+   s|\$REQUIRE_EMAIL|${REQUIRE_EMAIL}|g; \
+   s|\$DEMO|${DEMO}|g" \
+  /srv/runtime-config.js
 
 exec "$@"
