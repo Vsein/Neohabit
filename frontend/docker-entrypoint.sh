a26a5f838d58321557e01e291a99a57feea51b9f
Vsein <61903799+Vsein@users.noreply.github.com>
2026-02-12 12:28:50 +0400

    fix: remove persistence from volume frontend-dist:/srv

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -6,6 +6,8 @@ sed \
   s|$STRICT_USER_FIELDS|'${STRICT_USER_FIELDS}'|g; \
   s|$REQUIRE_EMAIL|'${REQUIRE_EMAIL}'|g; \
   s|$DEMO|'${DEMO}'|g' \
- /srv/runtime-config.template.js > /srv/runtime-config.js
+ /app-dist/runtime-config.template.js > /app-dist/runtime-config.js
+
+rm -rf /srv/* && cp -r /app-dist/* /srv/
 
 exec "$@"
